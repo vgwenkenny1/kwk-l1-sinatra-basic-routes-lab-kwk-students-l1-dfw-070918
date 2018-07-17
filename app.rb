67dafa00711ab71fdc3fc9c572bd621cfe485865
vgwenkenny1 <vgwenkenny1@gmail.com>
2018-07-17 16:47:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
-    "Hello, World!"
-  end
+  get '/name' do
+    "My name is"
+    get '/hometown' do 
+      "My hometown is"
+      get '/favorite-song' do
+        "My favorite song is"
 end 
